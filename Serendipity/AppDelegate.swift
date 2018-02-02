03d95f843d45f0cc3bfbf3e796ae325583228328
markcardamis <markncardamis@gmail.com>
2018-02-02 12:15:00 +1100

    Added default levels

--- Serendipity/AppDelegate.swift
+++ Serendipity/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UserDefaults.standard.register(defaults: ["identifier" : true])
+        UserDefaults.standard.set(true, forKey: "identifier")
         return true
     }
 
