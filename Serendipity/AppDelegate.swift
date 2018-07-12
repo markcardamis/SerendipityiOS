f38179c0d66d9d44d0ba657773b5014d845dea3b
markcardamis <markncardamis@gmail.com>
2018-07-12 19:16:51 +1000

    Remove default settings for levels and walkthrough

--- Serendipity/AppDelegate.swift
+++ Serendipity/AppDelegate.swift
@@ -17,8 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         UserDefaults.standard.register(defaults: ["identifier" : true])
         UserDefaults.standard.register(defaults: ["walkthrough" : false])
-        UserDefaults.standard.set(true, forKey: "identifier")
-        UserDefaults.standard.set(false, forKey: "walkthrough")
+        //UserDefaults.standard.set(true, forKey: "identifier")
+        //UserDefaults.standard.set(false, forKey: "walkthrough")
         return true
     }
 
